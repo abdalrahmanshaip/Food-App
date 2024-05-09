7fd7a70c008e905424f69e9008e1fba10c22b8a1
abdalrhman <abdalrahmanshaip@gmail.com>
2024-05-09 18:32:57 +0300

    Fixed Erorr

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/fontawesome-free/css/all.min.css"],
+    },
+  },
   plugins: [react()],
-  base: "/Food-App/"
-})
+  base: "/Food-App/",
+});
